7a1a52c76a3848abfd06c8fd8376b1cd2fa10c2f
emincaliskan3 <emincaliskan788@hotmail.com>
2024-09-04 02:41:39 +0300

    menutables_by_signalr

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -104,5 +104,11 @@ namespace SignalRApi.Hubs
 			var notificationListByFalse = _notificationService.TGetAllNotificationsByFalse();
 			await Clients.All.SendAsync("ReceiveNotificationListByFalse", notificationListByFalse);
 		}
+
+		public async Task GetMenuTableStatus()
+		{
+			var value = _menuTableService.TGetListAll();
+			await Clients.All.SendAsync("ReceiveMenuTableStatus", value);
+		}
 	}
 }
