f448d023fb8750cc0227cab59006d4a3fb8e188a
emincaliskan3 <emincaliskan788@hotmail.com>
2024-09-06 21:34:27 +0300

    update_bookingdto

--- SignalR.DtoLayer/BookingDto/CreateBookingDto.cs
+++ SignalR.DtoLayer/BookingDto/CreateBookingDto.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace SignalR.DtoLayer.BookingDto
 {
-    public class CreateBookingDto
-    {
-       
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Mail { get; set; }
-        public int PersonCount { get; set; }
-        public DateTime Date { get; set; }
-    }
+	public class CreateBookingDto
+	{
+
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		public string Phone { get; set; }
+		public string Mail { get; set; }
+		public int PersonCount { get; set; }
+		public DateTime Date { get; set; }
+	}
 }
--- SignalR.DtoLayer/BookingDto/ResultBookingDto.cs
+++ SignalR.DtoLayer/BookingDto/ResultBookingDto.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace SignalR.DtoLayer.BookingDto
 {
-    public class ResultBookingDto
-    {
-        public int BookingID { get; set; }
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Mail { get; set; }
-        public int PersonCount { get; set; }
-        public DateTime Date { get; set; }
-    }
+	public class ResultBookingDto
+	{
+		public int BookingID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Phone { get; set; }
+		public string Mail { get; set; }
+		public int PersonCount { get; set; }
+		public DateTime Date { get; set; }
+	}
 }
