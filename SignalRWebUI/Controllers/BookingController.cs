ec485ba3f760799124311ef88126ae1953ea2bc0
emincaliskan3 <emincaliskan788@hotmail.com>
2024-09-07 02:14:05 +0300

    api_booking_consume

--- SignalRWebUI/Controllers/BookingController.cs
+++ SignalRWebUI/Controllers/BookingController.cs
@@ -83,5 +83,19 @@ namespace SignalRWebUI.Controllers
 			}
 			return View();
 		}
+
+		public async Task<IActionResult> BookingStatusApproved(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			await client.GetAsync($"https://localhost:7193/api/Booking/BookingStatusApproved/{id}");
+			return RedirectToAction("Index");
+		}
+
+		public async Task<IActionResult> BookingStatusCancelled(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			await client.GetAsync($"https://localhost:7193/api/Booking/BookingStatusCancelled/{id}");
+			return RedirectToAction("Index");
+		}
 	}
 }
