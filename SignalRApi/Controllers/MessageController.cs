4c0415981516691566e24ef549766f99ea55ad41
emincaliskan3 <emincaliskan788@hotmail.com>
2024-09-11 00:06:43 +0300

    add_message_class

---
+++ SignalRApi/Controllers/MessageController.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MessageDto;
+using SignalR.EntityLayer.Entities;
--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -5,24 +5,24 @@ using SignalRWebUI.Dtos.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
-    public class _DefaultOfferComponentPartial : ViewComponent
-    {
-        private readonly IHttpClientFactory _httpClientFactory;
+	public class _DefaultOfferComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
 
-        public _DefaultOfferComponentPartial(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
+		public _DefaultOfferComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
 
-        public async Task<IViewComponentResult> InvokeAsync()
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7193/api/Discount");
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7193/api/Discount/GetListByStatusTrue");
 
-            var jsonData = await responseMessage.Content.ReadAsStringAsync();
-            var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
-            return View(values);
+			var jsonData = await responseMessage.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+			return View(values);
 
-        }
-    }
+		}
+	}
 }
