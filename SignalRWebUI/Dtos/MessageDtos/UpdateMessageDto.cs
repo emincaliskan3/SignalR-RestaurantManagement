e0c6305065bc4ea3186fd95e299f29fbcac5648d
emincaliskan3 <emincaliskan788@hotmail.com>
2024-09-11 01:46:46 +0300

    sendmessage_View

--- SignalRWebUI/Dtos/MessageDtos/UpdateMessageDto.cs
+++ SignalRWebUI/Dtos/MessageDtos/UpdateMessageDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SignalRWebUI.Dtos.MessageDto
+namespace SignalRWebUI.Dtos.MessageDtos
 {
 	public class UpdateMessageDto
 	{
