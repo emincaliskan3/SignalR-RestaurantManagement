8d2b70cc7cf8c23f33c7b8c777a7f1665e310145
emincaliskan3 <emincaliskan788@hotmail.com>
2024-09-13 21:29:51 +0300

    settingcontroller_post

--- SignalRWebUI/Controllers/SettingController.cs
+++ SignalRWebUI/Controllers/SettingController.cs
@@ -24,5 +24,22 @@ namespace SignalRWebUI.Controllers
             userEditDto.Mail = values.Email;
             return View(userEditDto);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditDto userEditDto)
+        {
+            if (userEditDto.Password == userEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Name = userEditDto.Name;
+                user.Surname = userEditDto.Surname;
+                user.Email = userEditDto.Mail;
+                user.UserName = userEditDto.Username;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditDto.Password);
+                await _userManager.UpdateAsync(user);
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
     }
 }
