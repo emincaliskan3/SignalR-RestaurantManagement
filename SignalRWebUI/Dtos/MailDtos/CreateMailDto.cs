89676296fdca184e8afb8bccc9d595cfd5857587
emincaliskan3 <emincaliskan788@hotmail.com>
2024-09-13 21:40:00 +0300

    createmaildto

---
+++ SignalRWebUI/Dtos/MailDtos/CreateMailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRWebUI.Dtos.MailDtos
+{
+    public class CreateMailDto
+    {
+        public int ReceiverMail { get; set; }
+        public int Subject { get; set; }
+        public int Body { get; set; }
+    }
+}
