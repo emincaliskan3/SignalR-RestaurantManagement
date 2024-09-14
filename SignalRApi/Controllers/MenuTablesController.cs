554a070940da494c615d5b2bbb7a3d608542d1b4
emincaliskan3 <emincaliskan788@hotmail.com>
2024-09-14 03:45:37 +0300

    menutable-message_controller-mapper

--- SignalRApi/Controllers/MenuTablesController.cs
+++ SignalRApi/Controllers/MenuTablesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.DtoLayer.MenuTableDto;
@@ -6,64 +7,57 @@ using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class MenuTablesController : ControllerBase
-	{
-		private readonly IMenuTableService _menuTableService;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuTablesController : ControllerBase
+    {
+        private readonly IMenuTableService _menuTableService;
+        private readonly IMapper _mapper;
 
-		public MenuTablesController(IMenuTableService menuTableService)
-		{
-			_menuTableService = menuTableService;
-		}
-		[HttpGet("MenuTableCount")]
-		public IActionResult MenuTableCount()
-		{
-			return Ok(_menuTableService.TMenuTableCount());
-		}
-		[HttpGet]
-		public IActionResult MenuTableList()
-		{
-			var values = _menuTableService.TGetListAll();
-			return Ok(values);
-		}
-		[HttpPost]
-		public IActionResult CreateMenuTable(CreateMenuTableDto createMenuTableDto)
-		{
-			MenuTable menuTable = new MenuTable()
-			{
-				Name = createMenuTableDto.Name,
-				Status = false
+        public MenuTablesController(IMenuTableService menuTableService, IMapper mapper)
+        {
+            _menuTableService = menuTableService;
+            _mapper = mapper;
+        }
+        [HttpGet("MenuTableCount")]
+        public IActionResult MenuTableCount()
+        {
+            return Ok(_menuTableService.TMenuTableCount());
+        }
+        [HttpGet]
+        public IActionResult MenuTableList()
+        {
+            var values = _menuTableService.TGetListAll();
+            return Ok(_mapper.Map<List<ResultMenuTableDto>>(values));
+        }
+        [HttpPost]
+        public IActionResult CreateMenuTable(CreateMenuTableDto createMenuTableDto)
+        {
+            createMenuTableDto.Status = false;
+            var value = _mapper.Map<MenuTable>(createMenuTableDto);
+            _menuTableService.TAdd(value);
+            return Ok("Masa Başarılı Bir Şekilde Eklendi");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMenuTable(int id)
+        {
+            var value = _menuTableService.TGetByID(id);
+            _menuTableService.TDelete(value);
+            return Ok("Masa Silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateMenuTable(UpdateMenuTableDto updateMenuTableDto)
+        {
+            var value = _mapper.Map<MenuTable>(updateMenuTableDto);
+            _menuTableService.TUpdate(value);
+            return Ok("Masa Güncellendi");
 
-			};
-			_menuTableService.TAdd(menuTable);
-			return Ok("Masa Başarılı Bir Şekilde Eklendi");
-		}
-		[HttpDelete("{id}")]
-		public IActionResult DeleteMenuTable(int id)
-		{
-			var value = _menuTableService.TGetByID(id);
-			_menuTableService.TDelete(value);
-			return Ok("Masa Silindi");
-		}
-		[HttpPut]
-		public IActionResult UpdateMenuTable(UpdateMenuTableDto updateMenuTableDto)
-		{
-			MenuTable menuTable = new MenuTable()
-			{
-				MenuTableID = updateMenuTableDto.MenuTableID,
-				Name = updateMenuTableDto.Name,
-				Status = false
-			};
-			_menuTableService.TUpdate(menuTable);
-			return Ok("Masa Güncellendi");
-
-		}
-		[HttpGet("{id}")]
-		public IActionResult GetMenuTable(int id)
-		{
-			var value = _menuTableService.TGetByID(id);
-			return Ok(value);
-		}
-	}
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetMenuTable(int id)
+        {
+            var value = _menuTableService.TGetByID(id);
+            return Ok(_mapper.Map<GetMenuTableDto>(value));
+        }
+    }
 }
--- SignalRApi/Controllers/MessageController.cs
+++ SignalRApi/Controllers/MessageController.cs
@@ -1,74 +1,60 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
 using SignalR.DtoLayer.MessageDto;
 using SignalR.EntityLayer.Entities;
 
+
 namespace SignalRApi.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class MessageController : ControllerBase
-	{
-		private readonly IMessageService _messageService;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+        private readonly IMapper _mapper;
 
-		public MessageController(IMessageService messageService)
-		{
-			_messageService = messageService;
-		}
-		[HttpGet]
-		public IActionResult MessageList()
-		{
-			var values = _messageService.TGetListAll();
-			return Ok(values);
-		}
-		[HttpPost]
-		public IActionResult CreateMessage(CreateMessageDto createMessageDto)
-		{
-			Message message = new Message()
-			{
-				NameSurname = createMessageDto.NameSurname,
-				Mail = createMessageDto.Mail,
-				MessageContent = createMessageDto.MessageContent,
-				Phone = createMessageDto.Phone,
-				Status = false,
-				Subject = createMessageDto.Subject,
-				MessageSendDate = DateTime.Now
-			};
-			_messageService.TAdd(message);
-			return Ok("Mesaj Başarılı Bir Şekilde Gönderildi");
-		}
-		[HttpDelete("{id}")]
-		public IActionResult DeleteMessage(int id)
-		{
-			var value = _messageService.TGetByID(id);
-			_messageService.TDelete(value);
-			return Ok("Mesaj Alanı Silindi");
-		}
-		[HttpPut]
-		public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
-		{
-			Message message = new Message()
-			{
-				MessageID = updateMessageDto.MessageID,
-				NameSurname = updateMessageDto.NameSurname,
-				Mail = updateMessageDto.Mail,
-				MessageContent = updateMessageDto.MessageContent,
-				Phone = updateMessageDto.Phone,
-				Status = false,
-				Subject = updateMessageDto.Subject,
-				MessageSendDate = updateMessageDto.MessageSendDate
+        public MessageController(IMessageService messageService, IMapper mapper)
+        {
+            _messageService = messageService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+            var values = _messageService.TGetListAll();
+            return Ok(_mapper.Map<List<ResultMessageDto>>(values));
+        }
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+        {
+            createMessageDto.Status = false;
+            createMessageDto.MessageSendDate = DateTime.Now;
+            var value = _mapper.Map<Message>(createMessageDto);
+            _messageService.TAdd(value);
+            return Ok("Mesaj Başarılı Bir Şekilde Gönderildi");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = _messageService.TGetByID(id);
+            _messageService.TDelete(value);
+            return Ok("Mesaj Alanı Silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
+        {
+            var value = _mapper.Map<Message>(updateMessageDto);
+            _messageService.TUpdate(value);
+            return Ok("Mesaj Başarılı Bir Şekilde Güncellendi");
 
-			};
-			_messageService.TUpdate(message);
-			return Ok("Mesaj Başarılı Bir Şekilde Güncellendi");
-
-		}
-		[HttpGet("{id}")]
-		public IActionResult GetMessage(int id)
-		{
-			var value = _messageService.TGetByID(id);
-			return Ok(value);
-		}
-	}
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetMessage(int id)
+        {
+            var value = _messageService.TGetByID(id);
+            return Ok(_mapper.Map<GetByIdMessageDto>(value));
+        }
+    }
 }
