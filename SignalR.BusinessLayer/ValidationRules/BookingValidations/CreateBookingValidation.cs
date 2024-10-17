34caa2454bac8bcb9d15e42da0057ccb538fc114
emincaliskan3 <emincaliskan788@hotmail.com>
2024-10-17 21:18:33 +0300

    CreateBookingValidation

---
+++ SignalR.BusinessLayer/ValidationRules/BookingValidations/CreateBookingValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using SignalR.DtoLayer.BookingDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.ValidationRules.BookingValidations
+{
+    public class CreateBookingValidation : AbstractValidator<CreateBookingDto>
+    {
+        public CreateBookingValidation()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim Alanı Boş Geçilemez.");
+            RuleFor(x => x.Phone).NotEmpty().WithMessage("Telefon Boş Geçilemez.");
+            RuleFor(x => x.Mail).NotEmpty().WithMessage("Mail Boş Geçilemez.");
+            RuleFor(x => x.PersonCount).NotEmpty().WithMessage("Kişi Sayısı Boş Geçilemez.");
+            RuleFor(x => x.Date).NotEmpty().WithMessage("Tarih Boş Geçilemez lütfen tarih seçimi yapınız.");
+
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("Lütfen isim alanına en az 5 karakter giriniz.").MaximumLength(50).WithMessage("Lütfen isim alanına en fazla 50 karakter giriniz.");
+            RuleFor(x => x.Description).MaximumLength(500).WithMessage("Lütfen açıklama alanına en fazla 500 karakter giriniz.");
+
+            RuleFor(x => x.Mail).EmailAddress().WithMessage("Lütfen geçerli bir email adresi giriniz");
+        }
+    }
+}
